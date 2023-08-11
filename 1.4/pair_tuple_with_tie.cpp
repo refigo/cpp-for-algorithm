550a45a25045ecced6122a88d8f4a8604de30670
Refi Go <64242999+refigo@users.noreply.github.com>
2023-08-11 13:47:16 +0900

    FEAT: Practice pair and tuple

--- 1.4/pair_tuple_with_tie.cpp
+++ 1.4/pair_tuple_with_tie.cpp
@@ -5,13 +5,10 @@ tuple<int, int, int> tl;
 int a, b, c;
 int main(){
 	pi = {1, 2};
-	a = pi.first;
-	b = pi.second;
-	cout << a << " : " << b << "\n";
 	tl = make_tuple(1, 2, 3);
-	a = get<0>(tl);
-	b = get<1>(tl);
-	c  =get<2>(tl);
+	tie(a, b) = pi;
+	cout << a << " : " << b << "\n";
+	tie(a, b, c) = tl;
 	cout << a << " : " << b << " : " << c << "\n";
 	return 0;
 }
