2a14fef3547bb0459c72c15bbc97f8de07ac5481
Refi Go <64242999+refigo@users.noreply.github.com>
2023-08-12 20:31:01 +0900

    FEAT: Practice stack, queue and deque

---
+++ 1.8/queue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> q;
+int main(){
+	for(int i = 1; i <= 10; i++)q.push(i);
+	while (q.size()){
+		cout << q.front() << ' ';
+		q.pop();
+	}
+	return 0;
+}
