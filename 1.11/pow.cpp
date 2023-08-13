2c1882d7ec0d1c697e215cc900a59929d4c5bb47
Refi Go <64242999+refigo@users.noreply.github.com>
2023-08-14 08:07:49 +0900

    FEAT: Pratice pow and sqrt

--- 1.11/pow.cpp
+++ 1.11/pow.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int n = 16;
-	int ret = (int)sqrt(n);
-	cout << ret << '\n';
+	int n = 4;
+	int pow_2 = (int)pow(2, n);
+	cout << pow_2 << '\n';
 	return 0;
 }
